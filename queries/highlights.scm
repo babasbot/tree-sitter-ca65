20d91f343c2c0e5124ac769a9ee5877731c50da5
Luis Alfredo Lorenzo <luis@babas.bot>
2023-02-19 16:17:05 -0600

    Defines the highlights queries

---
+++ queries/highlights.scm
@@ -0,0 +1,82 @@
+; opc
+
+(adc_opc) @keyword
+(and_opc) @keyword
+(asl_opc) @keyword
+(bcc_opc) @keyword
+(bcs_opc) @keyword
+(beq_opc) @keyword
+(bit_opc) @keyword
+(bmi_opc) @keyword
+(bne_opc) @keyword
+(bpl_opc) @keyword
+(brk_opc) @keyword
+(bvc_opc) @keyword
+(bvs_opc) @keyword
+(clc_opc) @keyword
+(cld_opc) @keyword
+(cli_opc) @keyword
+(clv_opc) @keyword
+(cmp_opc) @keyword
+(cpx_opc) @keyword
+(cpy_opc) @keyword
+(dec_opc) @keyword
+(dex_opc) @keyword
+(dey_opc) @keyword
+(eor_opc) @keyword
+(inc_opc) @keyword
+(inx_opc) @keyword
+(iny_opc) @keyword
+(jmp_opc) @keyword
+(jsr_opc) @keyword
+(lda_opc) @keyword
+(ldx_opc) @keyword
+(ldy_opc) @keyword
+(lsr_opc) @keyword
+(nop_opc) @keyword
+(ora_opc) @keyword
+(pha_opc) @keyword
+(php_opc) @keyword
+(pla_opc) @keyword
+(plp_opc) @keyword
+(rol_opc) @keyword
+(ror_opc) @keyword
+(rti_opc) @keyword
+(rts_opc) @keyword
+(sbc_opc) @keyword
+(sec_opc) @keyword
+(sed_opc) @keyword
+(sei_opc) @keyword
+(sta_opc) @keyword
+(stx_opc) @keyword
+(sty_opc) @keyword
+(tax_opc) @keyword
+(tay_opc) @keyword
+(tsx_opc) @keyword
+(txa_opc) @keyword
+(tcs_opc) @keyword
+(tya_opc) @keyword
+
+; numbers
+
+(hex_num_8)  @keyword
+(dec_num_8)  @keyword
+(bin_num_8)  @keyword
+(hex_num_16) @keyword
+(dec_num_16) @keyword
+(bin_num_16) @keyword
+
+(comment) @comment
+
+; punctuation
+
+(l_parentheis) @punctuation.bracket
+(r_parentheis) @punctuation.bracket
+(comma)        @punctuation.delimiter
+(imm_prefix)   @punctuation.special
+
+; registers
+
+(a_reg) @constant.builtin
+(x_reg) @constant.builtin
+(y_reg) @constant.builtin
