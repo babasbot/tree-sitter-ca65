1203b9f8b110a7d63c82e712e188a893e62969a4
Luis Alfredo Lorenzo <luis@babas.bot>
2023-03-03 20:07:15 -0600

    Fixes the highlights queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,72 +1,73 @@
 ; opc
 
 [
- (adc_opc)
- (and_opc)
- (asl_opc)
- (bcc_opc)
- (bcs_opc)
- (beq_opc)
- (bit_opc)
- (bmi_opc)
- (bne_opc)
- (bpl_opc)
- (brk_opc)
- (bvc_opc)
- (bvs_opc)
- (clc_opc)
- (cld_opc)
- (cli_opc)
- (clv_opc)
- (cmp_opc)
- (cpx_opc)
- (cpy_opc)
- (dec_opc)
- (dex_opc)
- (dey_opc)
- (eor_opc)
- (inc_opc)
- (inx_opc)
- (iny_opc)
- (jmp_opc)
- (jsr_opc)
- (lda_opc)
- (ldx_opc)
- (ldy_opc)
- (lsr_opc)
- (nop_opc)
- (ora_opc)
- (pha_opc)
- (php_opc)
- (pla_opc)
- (plp_opc)
- (rol_opc)
- (ror_opc)
- (rti_opc)
- (rts_opc)
- (sbc_opc)
- (sec_opc)
- (sed_opc)
- (sei_opc)
- (sta_opc)
- (stx_opc)
- (sty_opc)
- (tax_opc)
- (tay_opc)
- (tsx_opc)
- (txa_opc)
- (tcs_opc)
- (tya_opc)
+  (adc_opc)
+  (and_opc)
+  (asl_opc)
+  (bcc_opc)
+  (bcs_opc)
+  (beq_opc)
+  (bit_opc)
+  (bmi_opc)
+  (bne_opc)
+  (bpl_opc)
+  (brk_opc)
+  (bvc_opc)
+  (bvs_opc)
+  (clc_opc)
+  (cld_opc)
+  (cli_opc)
+  (clv_opc)
+  (cmp_opc)
+  (cpx_opc)
+  (cpy_opc)
+  (dec_opc)
+  (dex_opc)
+  (dey_opc)
+  (eor_opc)
+  (inc_opc)
+  (inx_opc)
+  (iny_opc)
+  (jmp_opc)
+  (jsr_opc)
+  (lda_opc)
+  (ldx_opc)
+  (ldy_opc)
+  (lsr_opc)
+  (nop_opc)
+  (ora_opc)
+  (pha_opc)
+  (php_opc)
+  (pla_opc)
+  (plp_opc)
+  (rol_opc)
+  (ror_opc)
+  (rti_opc)
+  (rts_opc)
+  (sbc_opc)
+  (sec_opc)
+  (sed_opc)
+  (sei_opc)
+  (sta_opc)
+  (stx_opc)
+  (sty_opc)
+  (tax_opc)
+  (tay_opc)
+  (tsx_opc)
+  (txa_opc)
+  (tcs_opc)
+  (tya_opc)
 ] @keyword
 
 [
- "warning"
- "error"
- "ldwarning"
- "lderror"
+  "warning"
+  "error"
+  "ldwarning"
+  "lderror"
 ] @keyword
 
 ; numbers
+
 [
   (num_8)
   (num_16)
@@ -74,19 +75,18 @@
 ] @number
 
 (comment) @comment
-
 ; punctuation
 
 [
- "("
- ")"
+  "("
+  ")"
 ] @punctuation.bracket
 
 [
   ","
 ] @punctuation.delimiter
 
-(imm_prefix)   @symbol
+(imm_prefix) @symbol
 
 ; registers
 
@@ -99,38 +99,38 @@
 (label) @property
 
 (constant_assignment) @constant
-(label_assignment)    @constant
-
+(label_assignment) @constant
 ; operators
 
 [
- "!"
- "&"
- "&&"
- "*"
- "+"
- "-"
- "/"
- "<"
- "<<"
- "<="
- "<>"
- "="
- ">"
- ">="
- ">>"
- "^"
- "|"
- "||"
- "~"
+  "!"
+  "&"
+  "&&"
+  "*"
+  "+"
+  "-"
+  "/"
+  "<"
+  "<<"
+  "<="
+  "<>"
+  "="
+  ">"
+  ">="
+  ">>"
+  "^"
+  "|"
+  "||"
+  "~"
 ] @operator
 
 [
- (ctrl_cmd)
+  (ctrl_cmd)
 ] @function.macro
 
 [
- (pseudo_var)
+  (pseudo_var)
 ] @constant.macro
 
 (str) @string
+
