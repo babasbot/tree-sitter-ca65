d52ff01f8f8f47a4e85c3ae09c54653a4a74f945
Luis Alfredo Lorenzo <babasbot@gmail.com>
2024-07-21 10:22:56 -0600

    Highlights control commands

--- queries/ca65/highlights.scm
+++ queries/ca65/highlights.scm
@@ -97,6 +97,17 @@
   (y_register)
 ] @constant.builtin
 
+; control commands
+
+[
+  (a16_ctrl_cmd)
+  (a8_ctrl_cmd)
+  (addr_ctrl_cmd)
+  (align_ctrl_cmd)
+  (asciiz_ctrl_cmd)
+  (assert_ctrl_cmd)
+] @function.macro
+
 [
   (warning_keyword)
   (error_keyword)
